ba557717e89c96b19be2888987990aa8f3d48e19
Yuta Hoshino <y.hoshino25@gmail.com>
2021-01-18 02:34:45 +0900

    procfileを追加し、heroku:addon仕様

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,16 @@
-rails_root = File.expand_path('../../', __FILE__)
+# rails_root = File.expand_path('../../', __FILE__)
 
-worker_processes 2
-working_directory rails_root
+# worker_processes 2
+# working_directory rails_root
 
-listen "#{rails_root}/tmp/unicorn.sock"
-pid "#{rails_root}/tmp/unicorn.pid"
+# listen "#{rails_root}/tmp/unicorn.sock"
+# pid "#{rails_root}/tmp/unicorn.pid"
 
-stderr_path "#{rails_root}/log/unicorn_error.log"
-stdout_path "#{rails_root}/log/unicorn.logi"
+# stderr_path "#{rails_root}/log/unicorn_error.log"
+# stdout_path "#{rails_root}/log/unicorn.logi"
+
+require 'fileutils'
+listen '/tmp/nginx.socket'
+before_fork do |server,worker|
+    FileUtils.touch('/tmp/app-initialized')
+end
